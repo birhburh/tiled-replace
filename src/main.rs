6d3e0b23a1814e0d3b0c063e5d9128816ad88f94
Birh Burh <birhburh@gmail.com>
2024-01-10 00:33:40 +0100

    convert command finished
    made serde to convert to json and xml

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,36 @@
 use clap::{Parser, Subcommand};
+use csv::{self, Terminator};
 use quick_xml::de::from_str;
 use quick_xml::events::BytesDecl;
 use quick_xml::events::Event;
 use quick_xml::Writer;
+use serde::ser::SerializeSeq;
+use serde::ser::SerializeStruct;
+use serde::Deserializer;
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::io::Cursor;
+use std::marker::PhantomData;
 use std::path::PathBuf;
 
-mod serialize_as_string {
-    use csv::{self, Terminator};
-    use serde::{self, Deserialize, Deserializer, Serializer};
-    use std::fmt::Write;
-
-    fn print_type_of<T>(_: &T) {
-        println!("{}", std::any::type_name::<T>())
-    }
-
-    trait SerType {
-        fn ser_type() -> String;
-    }
-
-    impl<T: Write> SerType for quick_xml::se::Serializer<'_, '_, T> {
-        fn ser_type() -> String {
-            "XML".to_string()
-        }
-    }
-
-    impl<T: Write> SerType for serde_json::Serializer<T> {
-        fn ser_type() -> String {
-            "JSON".to_string()
-        }
-    }
-
-    pub fn serialize<S>(d: &Vec<Vec<u32>>, serializer: S) -> Result<S::Ok, S::Error>
+pub trait SerializationFormat {
+    fn serialize_data<'a, S>(items: &Vec<Vec<u32>>, serializer: S) -> Result<S::Ok, S::Error>
     where
-        S: Serializer,
+        S: serde::Serializer;
+    fn transform_name(name: &str) -> &str;
+    fn transform_vec_name(name: &str) -> &str;
+}
+
+pub struct XmlFormat;
+impl SerializationFormat for XmlFormat {
+    fn serialize_data<'a, S>(items: &Vec<Vec<u32>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
     {
-        print_type_of(&serializer);
         let mut res = String::new();
 
-        let len = d.len();
-        for (i, record) in d.iter().enumerate() {
+        let len = items.len();
+        for (i, record) in items.iter().enumerate() {
             let mut v = Vec::new();
             let mut w = csv::WriterBuilder::new()
                 .has_headers(false)
@@ -61,27 +50,44 @@ mod serialize_as_string {
         serializer.serialize_str(&res)
     }
 
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<Vec<u32>>, D::Error>
+    fn transform_name(name: &str) -> &str {
+        name
+    }
+
+    fn transform_vec_name(name: &str) -> &str {
+        let mut chars = name.chars();
+        chars.next_back();
+        chars.as_str()
+    }
+}
+
+pub struct JsonFormat;
+impl SerializationFormat for JsonFormat {
+    fn serialize_data<'a, S>(items: &Vec<Vec<u32>>, serializer: S) -> Result<S::Ok, S::Error>
     where
-        D: Deserializer<'de>,
+        S: serde::Serializer,
     {
-        let mut res: Vec<Vec<u32>> = Vec::new();
-        let s = String::deserialize(deserializer)?;
-        let s = s.split(",\n");
-        for s in s {
-            let mut r = csv::ReaderBuilder::new()
-                .has_headers(false)
-                .from_reader(s.as_bytes());
-            let vals = r.records();
-            let vals = vals.map(|v| v.unwrap());
-            let vals = vals.map(|v| {
-                v.iter()
-                    .map(|v| v.parse::<u32>().unwrap())
-                    .collect::<Vec<_>>()
-            });
-            res.append(&mut vals.collect::<Vec<_>>());
+        let mut ser = serializer.serialize_seq(None)?;
+        for row in items {
+            for cell in row {
+                ser.serialize_element(cell)?;
+            }
         }
-        Ok(res)
+        ser.end()
+    }
+
+    fn transform_name(name: &str) -> &str {
+        if name.starts_with("@") {
+            let mut chars = name.chars();
+            chars.next();
+            chars.as_str()
+        } else {
+            name
+        }
+    }
+
+    fn transform_vec_name<'a>(name: &str) -> &str {
+        name
     }
 }
 
@@ -116,30 +122,102 @@ struct Cli {
     in_place: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct Image {
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(bound = "T: SerializationFormat")]
+struct Image<T: SerializationFormat> {
     #[serde(rename = "@source")]
     source: String,
     #[serde(rename = "@width")]
     width: u32,
     #[serde(rename = "@height")]
     height: u32,
+    #[serde(skip)]
+    rest: PhantomData<T>,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct Export {
+impl<T> Serialize for Image<T>
+where
+    T: SerializationFormat,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut res = serializer.serialize_struct("image", 3)?;
+        res.serialize_field(T::transform_name("@source"), &self.source)?;
+        res.serialize_field(T::transform_name("@width"), &self.width)?;
+        res.serialize_field(T::transform_name("@height"), &self.height)?;
+        res.end()
+    }
+}
+
+impl From<Image<XmlFormat>> for Image<JsonFormat> {
+    fn from(image: Image<XmlFormat>) -> Self {
+        Image::<JsonFormat> {
+            source: image.source,
+            width: image.width,
+            height: image.height,
+            rest: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(bound = "T: SerializationFormat")]
+struct Export<T: SerializationFormat> {
     #[serde(rename = "@target")]
     target: String,
     #[serde(rename = "@format")]
     format: String,
+    #[serde(skip)]
+    rest: PhantomData<T>,
 }
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct EditorSettings {
-    export: Export,
+
+impl<T> Serialize for Export<T>
+where
+    T: SerializationFormat,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut res = serializer.serialize_struct("export", 2)?;
+        res.serialize_field(T::transform_name("@target"), &self.target)?;
+        res.serialize_field(T::transform_name("@format"), &self.format)?;
+        res.end()
+    }
+}
+
+impl From<Export<XmlFormat>> for Export<JsonFormat> {
+    fn from(export: Export<XmlFormat>) -> Self {
+        Export::<JsonFormat> {
+            target: export.target,
+            format: export.format,
+            rest: Default::default(),
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct TileSet {
+#[serde(bound = "T: SerializationFormat")]
+struct EditorSettings<T: SerializationFormat> {
+    export: Export<T>,
+    #[serde(skip)]
+    rest: PhantomData<T>,
+}
+
+impl From<EditorSettings<XmlFormat>> for EditorSettings<JsonFormat> {
+    fn from(editorsettings: EditorSettings<XmlFormat>) -> Self {
+        EditorSettings::<JsonFormat> {
+            export: editorsettings.export.into(),
+            rest: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(bound = "T: SerializationFormat")]
+struct TileSet<T: SerializationFormat> {
     #[serde(rename = "@firstgid")]
     firstgid: u32,
     #[serde(rename = "@name")]
@@ -152,19 +230,102 @@ struct TileSet {
     tilecount: u32,
     #[serde(rename = "@columns")]
     columns: u32,
-    image: Image,
+    image: Image<T>,
 }
 
-#[derive(Default, Debug, Serialize, Deserialize, PartialEq)]
-struct Data {
+impl From<TileSet<XmlFormat>> for TileSet<JsonFormat> {
+    fn from(tileset: TileSet<XmlFormat>) -> Self {
+        TileSet::<JsonFormat> {
+            firstgid: tileset.firstgid,
+            name: tileset.name,
+            tilewidth: tileset.tilewidth,
+            tileheight: tileset.tileheight,
+            tilecount: tileset.tilecount,
+            columns: tileset.columns,
+            image: tileset.image.into(),
+        }
+    }
+}
+
+impl<T> Serialize for TileSet<T>
+where
+    T: SerializationFormat,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut res = serializer.serialize_struct("tileset", 7)?;
+        res.serialize_field(T::transform_name("@firstgid"), &self.firstgid)?;
+        res.serialize_field(T::transform_name("@name"), &self.name)?;
+        res.serialize_field(T::transform_name("@tilewidth"), &self.tilewidth)?;
+        res.serialize_field(T::transform_name("@tileheight"), &self.tileheight)?;
+        res.serialize_field(T::transform_name("@tilecount"), &self.tilecount)?;
+        res.serialize_field(T::transform_name("@columns"), &self.columns)?;
+        res.serialize_field("image", &self.image)?;
+        res.end()
+    }
+}
+
+#[derive(Default, Debug, Deserialize, PartialEq)]
+#[serde(bound = "T: SerializationFormat")]
+struct Data<T: SerializationFormat> {
     #[serde(rename = "@encoding")]
     encoding: String,
-    #[serde(rename = "$text", with = "serialize_as_string")]
+    #[serde(rename = "$text", deserialize_with = "deserialize_csv")]
     data: Vec<Vec<u32>>,
+    #[serde(skip)]
+    rest: PhantomData<T>,
 }
 
-#[derive(Default, Debug, Serialize, Deserialize, PartialEq)]
-struct Layer {
+pub fn deserialize_csv<'de, D>(deserializer: D) -> Result<Vec<Vec<u32>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let mut res: Vec<Vec<u32>> = Vec::new();
+    let s = String::deserialize(deserializer)?;
+    let s = s.split(",\n");
+    for s in s {
+        let mut r = csv::ReaderBuilder::new()
+            .has_headers(false)
+            .from_reader(s.as_bytes());
+        let vals = r.records();
+        let vals = vals.map(|v| v.unwrap());
+        let vals = vals.map(|v| {
+            v.iter()
+                .map(|v| v.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        });
+        res.append(&mut vals.collect::<Vec<_>>());
+    }
+    Ok(res)
+}
+
+impl<T> Serialize for Data<T>
+where
+    T: SerializationFormat,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        T::serialize_data(&self.data, serializer)
+    }
+}
+
+impl From<Data<XmlFormat>> for Data<JsonFormat> {
+    fn from(data: Data<XmlFormat>) -> Self {
+        Data::<JsonFormat> {
+            encoding: data.encoding,
+            data: data.data,
+            rest: Default::default(),
+        }
+    }
+}
+
+#[derive(Default, Debug, Deserialize, PartialEq)]
+#[serde(bound = "T: SerializationFormat")]
+struct Layer<T: SerializationFormat> {
     #[serde(rename = "@id")]
     id: Option<u32>,
     #[serde(rename = "@name")]
@@ -177,11 +338,46 @@ struct Layer {
     offsetx: u32,
     #[serde(rename = "@offsety", default)]
     offsety: u32,
-    data: Data,
+    data: Data<T>,
 }
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-struct Map {
+impl<T> Serialize for Layer<T>
+where
+    T: SerializationFormat,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut res = serializer.serialize_struct("layer", 7)?;
+        res.serialize_field(T::transform_name("@id"), &self.id)?;
+        res.serialize_field(T::transform_name("@name"), &self.name)?;
+        res.serialize_field(T::transform_name("@width"), &self.width)?;
+        res.serialize_field(T::transform_name("@height"), &self.height)?;
+        res.serialize_field(T::transform_name("@offsetx"), &self.offsetx)?;
+        res.serialize_field(T::transform_name("@offsety"), &self.offsety)?;
+        res.serialize_field("data", &self.data)?;
+        res.end()
+    }
+}
+
+impl From<Layer<XmlFormat>> for Layer<JsonFormat> {
+    fn from(layer: Layer<XmlFormat>) -> Self {
+        Layer::<JsonFormat> {
+            id: layer.id,
+            name: layer.name,
+            width: layer.width,
+            height: layer.height,
+            offsetx: layer.offsetx,
+            offsety: layer.offsety,
+            data: layer.data.into(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq)]
+#[serde(bound = "T: SerializationFormat")]
+struct Map<T: SerializationFormat> {
     #[serde(rename = "@version")]
     version: String,
     #[serde(rename = "@tiledversion")]
@@ -206,21 +402,82 @@ struct Map {
     nextlayerid: Option<u32>,
     #[serde(rename = "@nextobjectid")]
     nextobjectid: u32,
-    editorsettings: Option<EditorSettings>,
-    tileset: TileSet,
-    layer: Vec<Layer>,
+    editorsettings: Option<EditorSettings<T>>,
+    #[serde(rename = "tileset")]
+    tilesets: Vec<TileSet<T>>,
+    #[serde(rename = "layer")]
+    layers: Vec<Layer<T>>,
+}
+
+impl<T> Serialize for Map<T>
+where
+    T: SerializationFormat,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut res = serializer.serialize_struct("map", 15)?;
+        res.serialize_field(T::transform_name("@version"), &self.version)?;
+        res.serialize_field(T::transform_name("@tiledversion"), &self.tiledversion)?;
+        res.serialize_field(T::transform_name("@orientation"), &self.orientation)?;
+        res.serialize_field(T::transform_name("@renderorder"), &self.renderorder)?;
+        res.serialize_field(T::transform_name("@width"), &self.width)?;
+        res.serialize_field(T::transform_name("@height"), &self.height)?;
+        res.serialize_field(T::transform_name("@tilewidth"), &self.tilewidth)?;
+        res.serialize_field(T::transform_name("@tileheight"), &self.tileheight)?;
+        res.serialize_field(T::transform_name("@infinite"), &self.infinite)?;
+        res.serialize_field(T::transform_name("@backgroundcolor"), &self.backgroundcolor)?;
+        res.serialize_field(T::transform_name("@nextlayerid"), &self.nextlayerid)?;
+        res.serialize_field(T::transform_name("@nextobjectid"), &self.nextobjectid)?;
+        res.serialize_field("editorsettings", &self.editorsettings)?;
+        res.serialize_field(T::transform_vec_name("tilesets"), &self.tilesets)?;
+        res.serialize_field(T::transform_vec_name("layers"), &self.layers)?;
+        res.end()
+    }
+}
+
+impl From<Map<XmlFormat>> for Map<JsonFormat> {
+    fn from(map: Map<XmlFormat>) -> Self {
+        let editorsettings = if let Some(editorsettings) = map.editorsettings {
+            Some(editorsettings.into())
+        } else {
+            None
+        };
+        let tilesets = map.tilesets.into_iter().map(|x| x.into()).collect();
+        let layers = map.layers.into_iter().map(|x| x.into()).collect();
+        Map::<JsonFormat> {
+            version: map.version,
+            tiledversion: map.tiledversion,
+            orientation: map.orientation,
+            renderorder: map.renderorder,
+            width: map.width,
+            height: map.height,
+            tilewidth: map.tilewidth,
+            tileheight: map.tileheight,
+            infinite: map.infinite,
+            backgroundcolor: map.backgroundcolor,
+            nextlayerid: map.nextlayerid,
+            nextobjectid: map.nextobjectid,
+            editorsettings,
+            tilesets,
+            layers,
+        }
+    }
 }
 
 fn main() {
     let cli = Cli::parse();
 
     let contents = fs::read_to_string(cli.file).expect("Should have been able to read the file");
-    let mut map: Map = from_str(&contents).unwrap();
+    let mut map: Map<XmlFormat> = from_str(&contents).unwrap();
     if cli.command == Commands::Convert {
+        let map: Map<JsonFormat> = map.into();
         let res = serde_json::to_string_pretty(&map).unwrap();
-        println!("{}", res);
+        println!("{res}");
     } else {
-        for layer in &mut map.layer {
+        let tileset = map.tilesets.iter_mut().next().expect("Needs at least one tileset");
+        for layer in &mut map.layers {
             for row in &mut layer.data.data.iter_mut() {
                 for cell in row.iter_mut() {
                     match cli.command {
@@ -229,10 +486,10 @@ fn main() {
                                 *cell = replace + 1;
                             }
                         }
-                        Commands::Resize { tilecount, columns } => {
-                            if *cell >= map.tileset.columns {
-                                *cell += (*cell - 1) / map.tileset.columns
-                                    * (columns - map.tileset.columns);
+                        Commands::Resize { columns, .. } => {
+                            if *cell >= tileset.columns {
+                                *cell += (*cell - 1) / tileset.columns
+                                    * (columns - tileset.columns);
                             }
                         }
                         _ => (),
@@ -240,15 +497,12 @@ fn main() {
                 }
             }
         }
-        match cli.command {
-            Commands::Resize { tilecount, columns } => {
-                map.tileset.columns = columns;
-                map.tileset.tilecount = tilecount;
-            }
-            _ => (),
+        if let Commands::Resize { tilecount, columns } = cli.command {
+            tileset.columns = columns;
+            tileset.tilecount = tilecount;
         }
 
-        let mut writer = Writer::new_with_indent(Cursor::new(Vec::new()), ' ' as u8, 1);
+        let mut writer = Writer::new_with_indent(Cursor::new(Vec::new()), b' ', 1);
         writer
             .write_event(Event::Decl(BytesDecl::new("1.0", Some("UTF-8"), None)))
             .expect("cannot write xml header");
